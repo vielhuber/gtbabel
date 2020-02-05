6141ed13544f5c7fbda760acd8697b3ea0e2c989
David Vielhuber <david@vielhuber.de>
2020-02-05 03:45:07 +0100

    Fix typo

--- wordpress/gtbabel.php
+++ wordpress/gtbabel.php
@@ -9,7 +9,7 @@
  * License: free
  */
 
-if (file_exists(__DIR__ . '/vendor/scoper-autoload')) {
+if (file_exists(__DIR__ . '/vendor/scoper-autoload.php')) {
     require_once __DIR__ . '/vendor/scoper-autoload.php';
 } else {
     require_once __DIR__ . '/vendor/autoload.php';
