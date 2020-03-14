973a2511414f6f6894bf48449703ec07d690e209
David Vielhuber <david@vielhuber.de>
2020-03-14 02:55:12 +0100

    2.2.2

--- wordpress/deploy.sh
+++ wordpress/deploy.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+
+# save current folder
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# determine next version
+v=`git describe --abbrev=0 --tags 2>/dev/null`
+n=(${v//./ })
+n1=${n[0]}
+n2=${n[1]}
+n3=${n[2]}
+if [ -z "$n1" ] && [ -z "$n2" ] && [ -z "$n3" ]; then n1=1; n2=0; n3=0;else n3=$((n3+1)); fi
+if [ "$n3" == "10" ]; then n3=0; n2=$((n2+1)); fi
+if [ "$n2" == "10" ]; then n2=0; n1=$((n1+1)); fi
+v_new="$n1.$n2.$n3"
 
 # delete symlink that has been created when developing locally
 unlink vendor
@@ -10,6 +23,9 @@ cp -r ../src ./src
 cp ../helpers.php ./helpers.php
 composer install --no-dev
 composer update --no-dev
+
+# increase version number in readme.txt
+sed -i -e "s/Stable tag: [0-9]\.[0-9]\.[0-9]/Stable tag: $v_new/" ./readme.txt
 
 # do the prefixing with php-scoper
 wget https://github.com/humbug/php-scoper/releases/download/0.13.1/php-scoper.phar
@@ -22,16 +38,29 @@ sleep 3
 
 # rename and zip the build directory
 mv ./build/ ./gtbabel/
-zip -r ./gtbabel.zip ./gtbabel -x "gtbabel/composer.lock" -x "gtbabel/php-scoper.phar" -x "gtbabel/deploy.sh" -x "gtbabel/composer.json" -x "gtbabel/scoper.inc.php" -x \*"gtbabel/locales/"\* -x \*"gtbabel/logs/"\*
-rm -rf ./gtbabel
+rm -f ./gtbabel/composer.lock
+rm -f ./gtbabel/php-scoper.phar
+rm -f ./gtbabel/deploy.sh
+rm -f ./gtbabel/composer.json
+rm -f ./gtbabel/scoper.inc.php
+rm -rf ./gtbabel/locales/
+rm -rf ./gtbabel/logs/
+zip -r ./gtbabel.zip ./gtbabel
 
-# increase version number
-# TODO
-
-# add to subversion (or use github actions?)
-# TODO
+# add to subversion
+mkdir svn
+cd ./svn
+svn co https://plugins.svn.wordpress.org/gtbabel .
+rm -rf ./trunk/*
+cp -r ./../gtbabel/. ./trunk/
+svn add ./trunk/*
+svn cp ./trunk ./tags/$v_new
+svn ci -m "$v_new" --username vielhuber --password "XXXXXXXXX"
+cd $SCRIPT_DIR
 
 # remove obsolete files
+rm -rf ./svn/
+rm -rf ./gtbabel/
 rm -f ./php-scoper.phar
 rm -rf ./vendor/
 rm -rf ./src
@@ -41,3 +70,6 @@ rm -f ./composer.lock
 
 # reestablish symlink
 ln -s ../vendor ./vendor
+
+# git push + tag
+git add -A . && git commit -m "$v_new" && git push origin HEAD && git tag -a $v_new -m "$v_new" && git push --tags
