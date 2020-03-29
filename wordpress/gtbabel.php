fc1f2f1b01d5c5b5128c5a03730d5b356184f599
David Vielhuber <david@vielhuber.de>
2020-03-29 03:09:06 +0200

    2.5.2

--- wordpress/gtbabel.php
+++ wordpress/gtbabel.php
@@ -3,7 +3,7 @@
  * Plugin Name: Gtbabel
  * Plugin URI: https://github.com/vielhuber/gtbabel
  * Description: Instant server-side translation of any page.
- * Version: 2.4.8
+ * Version: 2.5.2
  * Author: David Vielhuber
  * Author URI: https://vielhuber.de
  * License: free
