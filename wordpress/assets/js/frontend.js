65b8eff1e0ecaac39846229e1493c50735d82f77
David Vielhuber <david@vielhuber.de>
2020-05-25 03:54:29 +0200

    Update iframe

--- wordpress/assets/js/frontend.js
+++ wordpress/assets/js/frontend.js
@@ -11,6 +11,18 @@ ready.then(() => {
     document.addEventListener('click', e => {
         let el = e.target.closest('#wp-admin-bar-gtbabel-frontend-editor .ab-item');
         if (el) {
+            if (document.querySelector('.gtbabel-frontend-editor') === null) {
+                document.body.insertAdjacentHTML(
+                    'beforeend',
+                    `
+                    <div class="gtbabel-frontend-editor">
+                        <iframe class="gtbabel-frontend-editor__iframe"></iframe>
+                    </div>
+                    `
+                );
+            }
+            document.querySelector('.gtbabel-frontend-editor__iframe').setAttribute('src', el.getAttribute('href'));
+
             e.preventDefault();
         }
     });
