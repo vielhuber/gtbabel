f318e85520470f336b8cae5d874f530a2f6e4423
David Vielhuber <david@vielhuber.de>
2020-10-09 02:57:02 +0200

    Setup analyze script

---
+++ tests/analyzer/analyze.php
@@ -0,0 +1,20 @@
+<?php
+if( !file_exists('log.txt') ) { die('log missing'); }
+$content = file_get_contents('log.txt');
+$content = explode('##############################################', $content);
+foreach(array_diff(scandir('.'), array('.', '..')) as $files__value) {
+    if( strpos($files__value, '.html') === false ) { continue; }
+    @unlink($files__value);
+}
+foreach($content as $content__key=>$content__value) {
+    if( $content__key % 2 === 0 ) { continue; }
+    $log = json_decode(trim($content__value));
+    $filename_result = (($content__key+1) / 2).'_result.html';
+    $filename_expected = (($content__key+1) / 2).'_expected.html';
+    file_put_contents($filename_result,$log[0]);
+    file_put_contents($filename_expected,$log[1]);
+    shell_exec('npx prettier --write '.$filename_result);
+    shell_exec('npx prettier --write '.$filename_expected);
+}
+echo 'done'.PHP_EOL;
+die();
--- tests/phpunit/files/in/23.php
+++ tests/phpunit/files/in/23.php
@@ -2,7 +2,7 @@
 <html>
     <body>
         <div>
-            <?php for ($i = 0; $i < 1000; $i++) {
+            <?php for ($i = 0; $i < 750; $i++) {
                 echo '
                     ok
                     <div>ok</div>
