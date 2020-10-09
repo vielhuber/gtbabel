dca24e4610be01ece975a1320d598a11240b25c8
David Vielhuber <david@vielhuber.de>
2020-10-09 02:00:15 +0200

    Replace travis ci logo

--- tests/phpunit/files/in/22.php
+++ tests/phpunit/files/in/22.php
@@ -2,14 +2,14 @@
 <html>
     <body>
         <div>
-            <?php for ($i = 0; $i < 1000; $i++) {
+            <?php for ($i = 0; $i < 750; $i++) {
                 echo '<strong>';
                 echo 'Dies ist ein ' . $i . ' Test!';
                 echo '</strong>';
             } ?>
         </div>
         <div>
-            <?php for ($i = 0; $i < 1000; $i++) {
+            <?php for ($i = 0; $i < 750; $i++) {
                 echo '<strong>';
                 echo 'Dies ist ein ' . $i . ' Test!';
                 echo '</strong>';
