d61f01d7d87b20ab8acf6f551d73db731754f8e1
David Vielhuber <david@vielhuber.de>
2020-10-09 03:17:30 +0200

    Setup analyze script

--- tests/analyzer/analyze.php
+++ tests/analyzer/analyze.php
@@ -17,10 +17,15 @@ foreach ($content as $content__key => $content__value) {
     $log = json_decode(trim($content__value));
     $filename_result = ($content__key + 1) / 2 . '_result.html';
     $filename_expected = ($content__key + 1) / 2 . '_expected.html';
+
+    $log[0] = str_replace(' ', PHP_EOL, $log[0]);
+    $log[1] = str_replace(' ', PHP_EOL, $log[1]);
+
     file_put_contents($filename_result, $log[0]);
     file_put_contents($filename_expected, $log[1]);
-    shell_exec('npx prettier --write ' . $filename_result);
-    shell_exec('npx prettier --write ' . $filename_expected);
+
+    //shell_exec('npx prettier --write ' . $filename_result);
+    //shell_exec('npx prettier --write ' . $filename_expected);
 }
 echo 'done' . PHP_EOL;
 die();
