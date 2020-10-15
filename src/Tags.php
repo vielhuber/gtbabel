253750feb8226fce2a598f0fa1ae316ee806112c
David Vielhuber <david@vielhuber.de>
2020-10-15 23:14:16 +0200

    4.7.8

--- src/Tags.php
+++ src/Tags.php
@@ -161,7 +161,7 @@ class Tags
         $prefix_pattern .= '|';
         $prefix_pattern .= '^((\d|[a-z])\))( +)'; // 1) 2) 3) a) b) c) etc.
         $prefix_pattern .= '|';
-        $prefix_pattern .= '^(\.\.\.|…)( *)'; // ...
+        $prefix_pattern .= '^(\.\.\.|…|&hellip;)( *)'; // ...
         $suffix_pattern = '';
         $suffix_pattern .= ' *<([a-zA-Z][a-zA-Z0-9]*)\b[^>]*>((\*|-|–|\||:|\+|•|●|,|I| | )*)<\/\1>$'; // <span>*</span> etc.
         $suffix_pattern .= '|';
@@ -169,7 +169,7 @@ class Tags
         $suffix_pattern .= '|';
         $suffix_pattern .= '( *)(\*|-|–|\||:|•|●)$'; // * etc.
         $suffix_pattern .= '|';
-        $suffix_pattern .= '( *)(\.\.\.|…)$'; // ...
+        $suffix_pattern .= '( *)(\.\.\.|…|&hellip;)$'; // ...
         $suffix_pattern .= '|';
         $suffix_pattern .= '( *)(: \d+)$'; // : ZAHL
         $prefix_matches = [0 => ['']];
